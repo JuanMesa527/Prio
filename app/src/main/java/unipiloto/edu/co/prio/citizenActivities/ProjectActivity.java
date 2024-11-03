c730ed5c144d6f86d2a96072e556b343f24bb55b
Michael bohorquez <128939308+MaicolB137@users.noreply.github.com>
2024-11-03 18:17:37 -0500

    commit 26/10

--- app/src/main/java/unipiloto/edu/co/prio/citizenActivities/ProjectActivity.java
+++ app/src/main/java/unipiloto/edu/co/prio/citizenActivities/ProjectActivity.java
@@ -2,6 +2,7 @@ package unipiloto.edu.co.prio.citizenActivities;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,11 +18,18 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import unipiloto.edu.co.prio.MainActivity;
 import unipiloto.edu.co.prio.MapsActivity;
@@ -33,8 +41,10 @@ public class ProjectActivity extends AppCompatActivity implements OnMapReadyCall
 
     private PrioDatabaseHelper dbHelper;
     private double lat, lng;
+    private PieChart pieChart;
     private String title;
     private Project item;
+    private boolean isVoteable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +67,7 @@ public class ProjectActivity extends AppCompatActivity implements OnMapReadyCall
         TextView datesTextView = findViewById(R.id.datesTextView);
         TextView categoryTextView = findViewById(R.id.categoryTextView);
         TextView localityTextView = findViewById(R.id.localityTextView);
+        pieChart = findViewById(R.id.pieChartPublic);
 
         item = getIntent().getParcelableExtra("item");
 
@@ -77,11 +88,50 @@ public class ProjectActivity extends AppCompatActivity implements OnMapReadyCall
         lng=Double.parseDouble(partes[1]);
         title = item.getTitle();
 
+        SharedPreferences sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE);
+        int userId = sharedPreferences.getInt("userId", -1);
 
-
+        isVoteable = dbHelper.isVoteable(userId,item.getId());
+        if (!isVoteable) {
+            fillPieChart();
+        }else {
+            pieChart.setVisibility(View.GONE);
+        }
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    public void fillPieChart() {
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        List<Integer> votes = dbHelper.getVotes(item.getId());
+        int totalVotes = votes.size();
+        int aFavor = 0;
+        int neutral = 0;
+        int enContra = 0;
+        for(int i : votes) {
+            if (i == 1) {
+                aFavor++;
+            } else if (i == 2) {
+                neutral++;
+            } else {
+                enContra++;
+            }
+        }
+        entries.add(new PieEntry(aFavor/(float)totalVotes*100, "A Favor"));
+        entries.add(new PieEntry(neutral/(float)totalVotes*100, "Indiferente"));
+        entries.add(new PieEntry(enContra/(float)totalVotes*100, "En Contra"));
+
+        PieDataSet pieDataSet = new PieDataSet(entries, "Votos");
+        pieDataSet.setColors(Color.GREEN, Color.YELLOW, Color.RED);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(16f);
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setUsePercentValues(true);
+        pieChart.setData(pieData);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setCenterText("Votos");
+        pieChart.animateY(2000);
     }
 
     @Override
@@ -129,10 +179,13 @@ public class ProjectActivity extends AppCompatActivity implements OnMapReadyCall
                 SharedPreferences sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE);
                 int userId = sharedPreferences.getInt("userId", -1);
 
-                boolean isVoteable = dbHelper.isVoteable(userId,item.getId());
+                isVoteable = dbHelper.isVoteable(userId,item.getId());
                 if (isVoteable) {
                     dbHelper.insertVote(userId, item.getId(), voteId, comment);
                     Toast.makeText(this, "Votación realizada con éxito", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(ProjectActivity.this, ProjectActivity.class);
+                    intent.putExtra("item", item);
+                    startActivity(intent);
                 } else {
                     Toast.makeText(this, "Ya ha votado por este proyecto", Toast.LENGTH_SHORT).show();
                 }
